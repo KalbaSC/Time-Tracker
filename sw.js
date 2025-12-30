02eca2a5a9495b7d9ad8845a71cb24cf28aeb590
KSC <rawandaldomour@gmail.com>
2025-12-30 11:00:29 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,3 +13,32 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
+const CACHE = "ksc-time-tracker-v1";
+const ASSETS = [
+  "./",
+  "./admin.html",
+  "./employee.html",
+  "./manifest.json",
+  "./manifest-admin.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./admin.html")))
+  );
+});
